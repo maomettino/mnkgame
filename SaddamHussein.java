1b73f535be2848dd363d36c7e3d415e3a58c616f
maomettino <aymen.tragha@gmail.com>
2021-11-19 11:04:40 +0100

    fixed the bugs

--- SaddamHussein.java
+++ SaddamHussein.java
@@ -7,17 +7,7 @@ import mnkgame.ChainState;
 import mnkgame.MNKCell;
 import mnkgame.MNKCellState;
 import mnkgame.MNKGameState;
-/*IMPORTANTE:
-l'algoritmo che controlla se io e l'avversario vinciamo in una mossa sono implementati
-e funzionano ma ci sono due problemi:
-1)viene usato B.B che è un campo protetto
-2)per poter selezionare mosse a nome dell'avversario bisogna cambiare
-CurrentPlayer(che è protetto) e l'unico modo per farlo è marcare prima una cella 
-a caso che non sia quella da testare.
-L'idea è di rimuovere la classe board e usare al suo posto una matrice locale
-e la funzione isWinningCell, che sono ciò che davvero ci serve della board
 
-*/
 public class SaddamHussein implements MNKPlayer {
 	private final int MAX_DEPTH = 5; // the max depth that the beta pruning will analyze
 	private int[][] direction = { 
@@ -31,14 +21,14 @@ public class SaddamHussein implements MNKPlayer {
 			{ -1, -1 } // up-left
 	};
 	private Random rand;
-	private MNKBoard B;
-
+	private  MNKCellState[][]    B;
 	private ChainState chain_state;
 	private int m, n, k, chain_length;
 	MNKCell knot_cell;
 	MNKCell current_best_move;
 	int turn;
-	MNKCellState whoami;
+	MNKCellState me;
+	MNKCellState foe;
 
 	/**
 	 * Default empty constructor
@@ -49,14 +39,18 @@ public class SaddamHussein implements MNKPlayer {
 	public void initPlayer(int M, int N, int K, boolean first, int timeout_in_secs) {
 		// New random seed for each game
 		rand = new Random(System.currentTimeMillis());
-		B = new MNKBoard(M, N, K);
+		B  = new MNKCellState[M][N];
+		for(int i = 0; i < M; i++)
+			for(int j = 0; j < N; j++)
+				B[i][j] = MNKCellState.FREE;
 		m = M;
 		n = N;
 		k = K;
 		chain_length = 0;
 		turn = 0;
 		chain_state = ChainState.nochain;
-		whoami = first ? MNKCellState.P1 : MNKCellState.P2;
+		me = first ? MNKCellState.P1 : MNKCellState.P2;
+		foe = first ? MNKCellState.P2 : MNKCellState.P1;
 	}
 
 	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
@@ -64,28 +58,26 @@ public class SaddamHussein implements MNKPlayer {
 		System.out.println("turno: " + turn);
 		
 		// When it's not my first turn
-		if (chain_state != ChainState.nochain) { // it's not my first move
+		if (chain_state != ChainState.nochain) { 
 			if(FC.length==1)
 				return FC[0];	
 			MNKCell my_last_cell = MC[MC.length - 2];
 			MNKCell foe_cell = MC[MC.length - 1]; 
 			//making the local board up-to-date
-			B.markCell(my_last_cell.i, my_last_cell.j);
-			B.markCell(foe_cell.i, foe_cell.j);
-			//printMatrix(B.B);			
+			B[my_last_cell.i][my_last_cell.j] = me;
+			B[foe_cell.i][foe_cell.j] = foe;		
 			//check if i can win in one move
-			/*if (turn >= k) {
+			if (turn >= k) {
 				int result[] = find_winning_move(my_last_cell);
 				if(result[0] !=-1)
 					return new MNKCell(result[0], result[1]);
-			}  */
+			}  
 			//check if the foe can win in one move
 			int foe_moves = MC.length - turn + 1;
 			if(foe_moves >= k-1) {
 				System.out.println("checking foe's moves...");
 				int result[] = find_winning_move(foe_cell);
 				if(result[0] !=-1) {	//i found a winning cell
-					System.out.println("the foe can win with "+ Arrays.toString(result)+", gotta stop him!");
 					return new MNKCell(result[0], result[1]);
 				}
 				System.out.println("no danger for now");
@@ -98,7 +90,7 @@ public class SaddamHussein implements MNKPlayer {
 			chain_state = ChainState.newborn;
 			if (MC.length > 0) { // if I'm the second player
 				MNKCell foe_cell = MC[MC.length - 1]; // Recover the last move from MC
-				B.markCell(foe_cell.i, foe_cell.j); // Save the last move in the local MNKBoard
+				B[foe_cell.i][foe_cell.j] = foe;				
 			}
 			return FC[0];
 		}
@@ -106,9 +98,7 @@ public class SaddamHussein implements MNKPlayer {
 	//returns the one-turn-winning move if it exists, returns -1,-1 if it doesn't
 	private int[] find_winning_move(MNKCell cell) {
 		for (int[] dir : direction) {
-			//System.out.println("checking direction " + Arrays.toString(dir));
 			int result[] = check_direction(cell.i, cell.j, dir,cell.state);
-			//System.out.println("result from find winning move " + Arrays.toString(result));
 			if(result[0] !=-1)	//i found a winning cell
 				return new int[] {result[0], result[1]};
 		}
@@ -117,24 +107,19 @@ public class SaddamHussein implements MNKPlayer {
 	//from the given cell moves in the given direction and returns the winning cell if it exists, [-1,-1 ] otherwise
 	private int[] check_direction(int i, int j, int[] dir, MNKCellState cellstate) {
 			//the cell is out of the board bound or it's marked by the other player	
-			System.out.println("i and j before" + i+j);
 			i += dir[0];
 			j += dir[1];
-			System.out.println("i and j after" + i+j);
-			if(i < 0 || i >= m || j < 0 || j >= n || (B.B[i][j] != MNKCellState.FREE && B.B[i][j] != cellstate ) )
+			if(i < 0 || i >= m || j < 0 || j >= n || (B[i][j] != MNKCellState.FREE && B[i][j] != cellstate ) )
 				return new int[] {-1,-1};
-			else if (B.B[i][j] == MNKCellState.FREE) {	//the cell is free
-				MNKGameState gamestate = B.markCell(i, j);
-				System.out.println("the gamestate for direction is "+gamestate);
-				if (gamestate == MNKGameState.WINP1 || gamestate == MNKGameState.WINP2) // i win with this																		// // move
+			else if (B[i][j] == MNKCellState.FREE) {	//the cell is free
+				Boolean win = isWinningCell(i, j, cellstate);
+				if(win)
 					return new int[] {i,j};
 				else {
-					B.unmarkCell();
 					return new int[] {-1,-1};
 				}
 			}
 			//recursive case
-			
 			return check_direction(i, j, dir,cellstate);
 	}
 	private void printMatrix(MNKCellState[][] matrix) {
@@ -144,42 +129,40 @@ public class SaddamHussein implements MNKPlayer {
 			System.out.println("]");
 		});
 	}
-/*
-	private boolean isWinningCell(int i, int j) {
-		MNKCellState s = B[i][j];
-		int n;
-
-		// Useless pedantic check
-		if(s == MNKCellState.FREE) return false;
-
+//marks the given cell for the given player and checks if he wins
+//does it really work?
+	private boolean isWinningCell(int i, int j, MNKCellState player) {
+		//can't check a cell if it's already marked
+		if(B[i][j] != MNKCellState.FREE) return false;
+		int c;
 		// Horizontal check
-		n = 1;
-		for(int k = 1; j-k >= 0 && B[i][j-k] == s; k++) n++; // backward check
-		for(int k = 1; j+k <  N && B[i][j+k] == s; k++) n++; // forward check   
-		if(n >= K) return true;
+		c = 1;
+		for(int k = 1; j-k >= 0 && B[i][j-k] == player; k++) c++; // backward check
+		for(int k = 1; j+k <  n && B[i][j+k] == player; k++) c++; // forward check   
+		if(c >= k) return true;
 
 		// Vertical check
-		n = 1;
-		for(int k = 1; i-k >= 0 && B[i-k][j] == s; k++) n++; // backward check
-		for(int k = 1; i+k <  M && B[i+k][j] == s; k++) n++; // forward check
-		if(n >= K) return true;
+		c = 1;
+		for(int k = 1; i-k >= 0 && B[i-k][j] == player; k++) c++; // backward check
+		for(int k = 1; i+k <  m && B[i+k][j] == player; k++) c++; // forward check
+		if(c >= k) return true;
 		
 
 		// Diagonal check
-		n = 1;
-		for(int k = 1; i-k >= 0 && j-k >= 0 && B[i-k][j-k] == s; k++) n++; // backward check
-		for(int k = 1; i+k <  M && j+k <  N && B[i+k][j+k] == s; k++) n++; // forward check
-		if(n >= K) return true;
+		c = 1;
+		for(int k = 1; i-k >= 0 && j-k >= 0 && B[i-k][j-k] == player; k++) c++; // backward check
+		for(int k = 1; i+k <  m && j+k <  n && B[i+k][j+k] == player; k++) c++; // forward check
+		if(c >= k) return true;
 
 		// Anti-diagonal check
-		n = 1;
-		for(int k = 1; i-k >= 0 && j+k < N  && B[i-k][j+k] == s; k++) n++; // backward check
-		for(int k = 1; i+k <  M && j-k >= 0 && B[i+k][j-k] == s; k++) n++; // backward check
-		if(n >= K) return true;
+		c = 1;
+		for(int k = 1; i-k >= 0 && j+k < n  && B[i-k][j+k] == player; k++) c++; // backward check
+		for(int k = 1; i+k <  m && j-k >= 0 && B[i+k][j-k] == player; k++) c++; // backward check
+		if(c >= k) return true;
 
 		return false;
 	}
-*/
+
 	public String playerName() {
 		return "SaddamHussein";
 	}
