a4995234de205985a635dafc5b0c3535e49df31f
maomettino <aymen.tragha@gmail.com>
2021-11-20 12:20:29 +0100

    starting beta-pruning implementation

--- SaddamHussein.java
+++ SaddamHussein.java
@@ -1,14 +1,13 @@
 package mnkgame;
-
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
-import mnkgame.ChainState;
-import mnkgame.MNKCell;
-import mnkgame.MNKCellState;
-import mnkgame.MNKGameState;
-
 public class SaddamHussein implements MNKPlayer {
+	//TODO: handle time with timer, threads or whatever
+	//TODO: look-up and perhaps use junit for unit testing
+	//TODO: reycle previously computed stuff in beta-pruning if possible
 	private final int MAX_DEPTH = 5; // the max depth that the beta pruning will analyze
 	private int[][] direction = { 
 			{ -1, 0 }, // up
@@ -29,6 +28,9 @@ public class SaddamHussein implements MNKPlayer {
 	int turn;
 	MNKCellState me;
 	MNKCellState foe;
+	//beta-pruning parameters
+	final int BETA = 1000;
+	final int ALPHA = -1000;
 
 	/**
 	 * Default empty constructor
@@ -55,8 +57,7 @@ public class SaddamHussein implements MNKPlayer {
 
 	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
 		turn++;
-		System.out.println("turno: " + turn);
-		
+		System.out.println("turno: " + turn);		
 		// When it's not my first turn
 		if (chain_state != ChainState.nochain) { 
 			if(FC.length==1)
@@ -82,7 +83,6 @@ public class SaddamHussein implements MNKPlayer {
 				}
 				System.out.println("no danger for now");
 			}
-			return FC[0];
 		}
 		// When it's my first turn
 		else
@@ -91,10 +91,80 @@ public class SaddamHussein implements MNKPlayer {
 			if (MC.length > 0) { // if I'm the second player
 				MNKCell foe_cell = MC[MC.length - 1]; // Recover the last move from MC
 				B[foe_cell.i][foe_cell.j] = foe;				
-			}
-			return FC[0];
+			}	
 		}
+		//here goes the beta-pruning algorithm
+		//return beta_pruning(order_moves(FC),true,ALPHA,BETA,0,0);
+		//order_moves(FC);
+		return FC[0];
 	}
+
+	private ArrayList<MNKCell> order_moves( MNKCell[] FC) {
+		List<MNKCell> list = Arrays.asList(FC); 
+		ArrayList<MNKCell> moves = new ArrayList<MNKCell>(list);
+		//System.out.println(moves.get(0).state);
+		//order moves according to heuristic
+		return moves;
+	}
+//beta_pruning should return MNKCell or int?
+	private int beta_pruning(ArrayList<MNKCell> moves, Boolean maximizingPlayer,int alpha, int beta, int nodeIndex, int depth ) {
+		//the moves array is already ordered, so we pick up the first move,
+		//then the second and so on inside the for
+		//i have another terminating condition which occurs when the move
+		//of the current node ends the game somehow, to do so 
+		//i must keep track of all the moves made in a root-leaf path in a copy
+		//of the local board
+		MNKCell c = moves.get(0);
+		if( isWinningCell(c.i,c.j,me) ) {
+			//maximum positive/negative value if i win/lose from root player pov
+			return 0;
+		}
+
+		// Terminating condition. i.e
+        // leaf node is reached
+        if (depth == MAX_DEPTH) {
+			//heuristic value for the node get_heuristic_value(moves[0])
+			return 0;
+		}
+            
+
+        if (maximizingPlayer) {
+            int best = ALPHA;
+
+            // Recur for left and
+            // right children
+			//this will be more likely a foreach through moves
+            for (int i = 0; i < moves.size(); i++) {
+				//remove the marked move from moves before the recursive call
+                int val = 0;//beta_pruning(depth + 1, nodeIndex * 2 + i, false, moves, alpha, beta);
+                best = Math.max(best, val);
+                alpha = Math.max(alpha, best);
+
+                // Alpha Beta Pruning
+                if (beta <= alpha)
+                    break;
+            }
+            return best;
+        } else {
+            int best = BETA;
+
+            // Recur for left and
+            // right children
+			
+            for (int i = 0; i < 2; i++) {
+
+                int val = 0;//beta_pruning(depth + 1, nodeIndex * 2 + i, true, moves, alpha, beta);
+                best = Math.min(best, val);
+                beta = Math.min(beta, best);
+
+                // Alpha Beta Pruning
+                if (beta <= alpha)
+                    break;
+            }
+            return best;
+        }		
+	}
+
 	//returns the one-turn-winning move if it exists, returns -1,-1 if it doesn't
 	private int[] find_winning_move(MNKCell cell) {
 		for (int[] dir : direction) {
@@ -104,6 +174,7 @@ public class SaddamHussein implements MNKPlayer {
 		}
 		return new int[] {-1,-1};
 	}
+
 	//from the given cell moves in the given direction and returns the winning cell if it exists, [-1,-1 ] otherwise
 	private int[] check_direction(int i, int j, int[] dir, MNKCellState cellstate) {
 			//the cell is out of the board bound or it's marked by the other player	
@@ -122,6 +193,7 @@ public class SaddamHussein implements MNKPlayer {
 			//recursive case
 			return check_direction(i, j, dir,cellstate);
 	}
+
 	private void printMatrix(MNKCellState[][] matrix) {
 		Arrays.stream(matrix).forEach((row) -> {
 			System.out.print("[");
@@ -129,8 +201,8 @@ public class SaddamHussein implements MNKPlayer {
 			System.out.println("]");
 		});
 	}
-//marks the given cell for the given player and checks if he wins
-//does it really work?
+
+	//marks the given cell for the given player and checks if he wins, but does it really work?
 	private boolean isWinningCell(int i, int j, MNKCellState player) {
 		//can't check a cell if it's already marked
 		if(B[i][j] != MNKCellState.FREE) return false;
